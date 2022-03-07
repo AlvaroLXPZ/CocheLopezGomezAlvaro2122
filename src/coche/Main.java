9f58e843b0f19f2eba5d7abd1fedfc8b4e7ece69
AlvaroLXPZ <84009732+AlvaroLXPZ@users.noreply.github.com>
2022-03-07 21:49:02 +0100

    Add files via upload

---
+++ src/coche/Main.java
@@ -0,0 +1,46 @@
+    
+package coche;
+
+/**
+ *
+ * @author Fran
+ */
+public class Main {
+    
+      public static void main(String[] args) {
+        Coche miCoche;
+        int stockActual;
+        
+        miCoche = new Coche("Opel",12000,500);
+        operatoriaVentaLopezGomezAlvaro2122(miCoche);
+        
+        operatoriaCompraLopezGomezAlvaro2122(miCoche);
+        stockActual = miCoche.obtenerStock();
+        System.out.println("El stock actual es"+ stockActual );
+    }
+
+    public static void operatoriaCompraLopezGomezAlvaro2122(Coche miCoche) {
+        try
+        {
+            System.out.println("Compra de Coches");
+            miCoche.comprar(500);
+        } catch (Exception e)
+        {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+
+    public static void operatoriaVentaLopezGomezAlvaro2122(Coche miCoche) {
+        try
+        {
+            System.out.println("Venta de Coches");
+            miCoche.vender(300);
+        } catch (Exception e)
+        {
+            System.out.print("Fallo al vender");
+        }
+    }
+
+}
+    
+// PRUEBA LOPEZGOMEZALVARO2122
